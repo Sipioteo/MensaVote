cd18a9f48abc13a9963e92608757f4ad88be9178
Sipioteo <matteo@sipio.it>
2023-05-07 09:47:41 +0200

    re

--- src/routes/auth/redirect/+page.server.js
+++ src/routes/auth/redirect/+page.server.js
@@ -6,18 +6,14 @@ export async function load({ url }) {
 		return {};
 	}
 
-	
 	const response = await fetch(
 		`${env.AUTH_URL || 'https://authmensa.luminos.dev'}/.well-known/openid-configuration`
 	);
 	const authEndpoints = await response.json();
 	const tokenBody = new URLSearchParams();
 	tokenBody.append('grant_type', 'authorization_code');
-	tokenBody.append('client_id', env.CLIENT_ID || '212205468813164598@website');
-	tokenBody.append(
-		'client_secret',
-		env.CLIENT_SECRET || 'ZhTynXTX2q5onLbwS0qhyFvh8LymZkw4ZyhQ71K7XE7mQfEZDLvMNP7wZShkLHEP'
-	);
+	tokenBody.append('client_id', env.CLIENT_ID || 'id');
+	tokenBody.append('client_secret', env.CLIENT_SECRET || 'secret');
 	tokenBody.append('redirect_uri', `${env.BASE_URL || 'http://localhost:5173'}/auth/redirect`);
 	tokenBody.append('code', url.searchParams.get('code') || '');
 
